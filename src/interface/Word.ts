d822ce637cab7fb4ac0d486426dd26a5e697b965
chelizichen <1347290221@qq.com>
2023-08-21 01:44:14 +0800

    update:优化部分功能

--- src/interface/Word.ts
+++ src/interface/Word.ts
@@ -1,6 +1,6 @@
 import {TarsusInterFace, TarsusMethod, Stream} from "tarsus/core/microservice";
 import {$PoolConn} from 'tarsus/core/database'
-
+import moment from 'moment'
 //
 import {
     getWordListReq,
@@ -12,6 +12,7 @@ import {
     Word,
     WordTranslate
 } from "../struct/Word";
+import {sign} from "crypto";
 
 
 interface WordServerInf {
@@ -32,24 +33,115 @@ class WordImpl implements WordServerInf {
     @Stream("getWordListReq", "getWordListRes")
     getWordList(Request: getWordListReq, Response: getWordListRes): Promise<getWordListRes> {
         return new Promise(async (resolve, reject) => {
+            const {page = 1, size = 10, keyword = '', start_time, desc = "1", end_time} = Request
+            const conn = await $PoolConn();
+            let current = (page - 1) * 10;
+            let where = ' where 1 = 1 '
+            let select = `
+select words.*,COUNT(word_translates.id) AS total_trans 
+from words 
+LEFT JOIN word_translates ON words.id = word_translates.word_id
+`
+            let total = 'select count(*) as total from words';
+            let limit = `limit ${current},${size}`
+            if (keyword) {
+                where += ` and en_name like %${keyword}% `
+            }
+            if (start_time && end_time) {
+                where += ` and create_time between ${start_time} and ${end_time} `
+            }
+            where += ` GROUP BY words.id order by create_time ${desc == "1" ? 'desc' : ''} `
 
-            resolve(Response)
+            let signal = {
+                total: false,
+                list: false
+            }
+
+            conn.query(total + where, function (_, resu) {
+                console.log('total',total + where)
+                console.log('total',resu[0])
+                // Response.total = resu[0].total;
+                Response.total = resu[0].total;
+                signal.total = true
+                if (signal.total && signal.list) {
+                    resolve(Response)
+                    conn.release()
+                }
+
+            })
+
+            conn.query(select + where + limit, function (_, resu) {
+                console.log('select',select + where + limit)
+                console.log('select',resu)
+                Response.list = resu.map(item=>{
+                    item.create_time = moment(item.create_time).format("YYYY-MM-DD HH:mm:ss")
+                    return item;
+                });
+                // Response.list = [];
+                signal.list = true
+                if (signal.total && signal.list) {
+                    resolve(Response)
+                    conn.release()
+                }
+            })
+
         })
     }
 
     @TarsusMethod
     @Stream("getTranslateListReq", "getTranslateListRes")
     getTranslateList(Request: getTranslateListReq, Response: getTranslateListRes): Promise<getTranslateListRes> {
-        return new Promise((resolve, reject) => {
-            resolve(Response)
+        return new Promise(async (resolve, reject) => {
+            const {page = 1, size = 10, keyword = '', start_time, desc = "1", end_time} = Request
+            const conn = await $PoolConn();
+            let current = (page - 1) * 10;
+            let where = ' where 1 = 1 and '
+            let select = 'select * from words'
+            let total = 'select (*) as total from words';
+            let limit = `limit${current},${size}`
+            if (keyword) {
+                where += ` en_name like %${keyword}% and `
+            }
+            if (start_time && end_time) {
+                where += `create_time between ${start_time} and ${end_time}`
+            }
+            where += `order by create_time ${desc == "1" ? 'desc' : ''}`
+
+            let signal = {
+                total: false,
+                list: false
+            }
+
+            conn.query(total + where, function (_, resu) {
+                Response.total = resu[0].total;
+                signal.total = true
+                if (signal.total && signal.list) {
+                    resolve(Response)
+                }
+
+            })
+
+            conn.query(select + where + limit, function (_, resu) {
+                Response.list = resu;
+                signal.list = true
+                if (signal.total && signal.list) {
+                    resolve(Response)
+                }
+            })
+
         })
     }
 
     @TarsusMethod
     @Stream("DelReq", "DelOrSaveRes")
     delWordById(Request: DelReq, Response: DelOrSaveRes): Promise<DelOrSaveRes> {
-        return new Promise((resolve, reject) => {
-            resolve(Response)
+        return new Promise(async (resolve, reject) => {
+            const conn = await $PoolConn();
+            let sql = `delete from words where id = ${Request.id}`;
+            conn.query(sql,function (){
+                Response.message = "删除成功";
+                resolve(Response)
+            })
         })
     }
 
@@ -62,18 +154,18 @@ class WordImpl implements WordServerInf {
             insert into words(en_name,create_time,own_mark,type)values(?,?,?,?)
         `
         return new Promise(async (resolve, reject) => {
-            // const data = await $Query(sql);
             const conn = await $PoolConn();
-            conn.query(sql,params,(err)=>{
-                if(err){
+            conn.query(sql, params, (err) => {
+                if (err) {
                     Response.code = 600
                     Response.message = "db insert error " + sql;
                     resolve(Response)
-                }else {
+                } else {
                     Response.code = 0;
-                    Response.message = "ok"
+                    Response.message = "插入成功"
                     resolve(Response)
                 }
+                conn.release();
             })
         })
     }
