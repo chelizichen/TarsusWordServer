eae91c04f44ccaf83b106ba6ad830dc9f08db88b
chelizichen <1347290221@qq.com>
2023-08-21 17:00:21 +0800

    update:计划服务更新

--- src/interface/PlanInf.ts
+++ src/interface/PlanInf.ts
@@ -1,9 +1,33 @@
-import {queryIdReq} from "../struct/User";
-import {getUserPlanByIdRes} from "../struct/Plan";
+import {baseRes, queryIdReq} from "../struct/User";
+import {
+    getPlanDetailByIdRes,
+    getPlanWordsByIdRes,
+    getUserPlanByIdRes,
+    getUserPlansRes,
+    PlanDetail, PlanWords
+} from "../struct/Plan";
 import {Stream, TarsusInterFace, TarsusMethod} from "tarsus/core/microservice";
 
 interface PlanInf {
-    getUserPlanById(Request: queryIdReq, Response: getUserPlanByIdRes): Promise<getUserPlanByIdRes>
+    getLatestPlanByUser(Request: queryIdReq, Response: getPlanDetailByIdRes): Promise<getPlanDetailByIdRes>
+
+    getPlansByUser(Request: queryIdReq, Response: getUserPlansRes): Promise<getUserPlansRes>
+
+    getPlanById(Request: queryIdReq, Response: getPlanDetailByIdRes): Promise<getPlanDetailByIdRes>
+
+    getPlanWordsById(Request: queryIdReq, Response: getPlanWordsByIdRes): Promise<getPlanWordsByIdRes>
+
+    markPlanWords(Request: queryIdReq, Response: baseRes): Promise<baseRes>
+
+    markPlan(Request: queryIdReq, Response: baseRes): Promise<baseRes>
+
+    delPlan(Request: queryIdReq, Response: baseRes): Promise<baseRes>
+
+    delPlanWords(Request: queryIdReq, Response: baseRes): Promise<baseRes>
+
+    savePlan(Request: PlanDetail, Response: baseRes): Promise<baseRes>
+
+    savePlanWords(Request: PlanWords, Response: baseRes): Promise<baseRes>
 }
 
 @TarsusInterFace("plan")
@@ -11,6 +35,66 @@ class PlanImpl implements PlanInf {
     @TarsusMethod
     @Stream("queryIdReq", "getUserPlanByIdRes")
     getUserPlanById(Request: queryIdReq, Response: getUserPlanByIdRes): Promise<getUserPlanByIdRes> {
+        return Promise.resolve(undefined);
+    }
+
+    @TarsusMethod
+    @Stream("queryIdReq", "baseRes")
+    delPlan(Request: queryIdReq, Response: baseRes): Promise<baseRes> {
+        return Promise.resolve(undefined);
+    }
+
+    @TarsusMethod
+    @Stream("queryIdReq", "baseRes")
+    delPlanWords(Request: queryIdReq, Response: baseRes): Promise<baseRes> {
+        return Promise.resolve(undefined);
+    }
+
+    @TarsusMethod
+    @Stream("queryIdReq", "getPlanDetailByIdRes")
+    getLatestPlanByUser(Request: queryIdReq, Response: getPlanDetailByIdRes): Promise<getPlanDetailByIdRes> {
+        return Promise.resolve(undefined);
+    }
+
+    @TarsusMethod
+    @Stream("queryIdReq", "getPlanDetailByIdRes")
+    getPlanById(Request: queryIdReq, Response: getPlanDetailByIdRes): Promise<getPlanDetailByIdRes> {
+        return Promise.resolve(undefined);
+    }
+
+    @TarsusMethod
+    @Stream("queryIdReq", "getPlanWordsByIdRes")
+    getPlanWordsById(Request: queryIdReq, Response: getPlanWordsByIdRes): Promise<getPlanWordsByIdRes> {
+        return Promise.resolve(undefined);
+    }
+
+    @TarsusMethod
+    @Stream("queryIdReq", "getPlanWordsByIdRes")
+    getPlansByUser(Request: queryIdReq, Response: getUserPlansRes): Promise<getUserPlansRes> {
+        return Promise.resolve(undefined);
+    }
+
+    @TarsusMethod
+    @Stream("queryIdReq", "baseRes")
+    markPlan(Request: queryIdReq, Response: baseRes): Promise<baseRes> {
+        return Promise.resolve(undefined);
+    }
+
+    @TarsusMethod
+    @Stream("queryIdReq", "baseRes")
+    markPlanWords(Request: queryIdReq, Response: baseRes): Promise<baseRes> {
+        return Promise.resolve(undefined);
+    }
+
+    @TarsusMethod
+    @Stream("PlanDetail", "baseRes")
+    savePlan(Request: PlanDetail, Response: baseRes): Promise<baseRes> {
+        return Promise.resolve(undefined);
+    }
+
+    @TarsusMethod
+    @Stream("PlanWords", "baseRes")
+    savePlanWords(Request: PlanWords, Response: baseRes): Promise<baseRes> {
         return Promise.resolve(undefined);
     }
 }
--- src/struct/Plan.ts
+++ src/struct/Plan.ts
@@ -2,46 +2,39 @@ import {User} from "./User";
 
 const {TarsusReadStream} = require("tarsus-cli/taro");
 
-export class Plan {
+export class PlanDetail {
     public id: number;
     public user_id: string;
-    public plan_mark: string;
+    public is_mark: string;
     public plan_start_time: string;
     public plan_end_time: string;
     public create_time: string;
 
     constructor(...args: any[]) {
-        const _TarsusReadStream = new TarsusReadStream("Plan", args);
+        const _TarsusReadStream = new TarsusReadStream("PlanDetail", args);
         this.id = _TarsusReadStream.read_int(1);
         this.user_id = _TarsusReadStream.read_string(2);
-        this.plan_mark = _TarsusReadStream.read_string(3);
+        this.is_mark = _TarsusReadStream.read_string(3);
         this.plan_start_time = _TarsusReadStream.read_string(4);
         this.plan_end_time = _TarsusReadStream.read_string(5);
         this.create_time = _TarsusReadStream.read_string(6);
     }
 };
 
-export class PlanDetail {
-    public id: number;
-    public plan_id: number;
-    public plan_dates: Array<PlanWords>;
-
-    constructor(...args: any[]) {
-        const _TarsusReadStream = new TarsusReadStream("PlanDetail", args);
-        this.id = _TarsusReadStream.read_int(1);
-        this.plan_id = _TarsusReadStream.read_int(2);
-        this.plan_dates = _TarsusReadStream.read_list(3, "List<PlanWords>");
-    }
-};
-
 export class PlanWords {
     public word_ids: Array<number>;
     public date: string;
+    public is_mark: number;
+    public user_id: string;
+    public plan_id: string;
 
     constructor(...args: any[]) {
         const _TarsusReadStream = new TarsusReadStream("PlanWords", args);
         this.word_ids = _TarsusReadStream.read_list(1, "List<int>");
         this.date = _TarsusReadStream.read_string(2);
+        this.is_mark = _TarsusReadStream.read_int(3);
+        this.user_id = _TarsusReadStream.read_string(4);
+        this.plan_id = _TarsusReadStream.read_string(5);
     }
 };
 
@@ -59,5 +52,46 @@ export class getUserPlanByIdRes {
         this.code = _TarsusReadStream.read_int(3);
         this.total = _TarsusReadStream.read_int(4);
         this.message = _TarsusReadStream.read_string(5);
+    }
+};
+
+export class getPlanDetailByIdRes {
+    public data: PlanDetail;
+    public code: number;
+    public message: string;
+
+    constructor(...args: any[]) {
+        const _TarsusReadStream = new TarsusReadStream("getPlanDetailByIdRes", args);
+        this.data = _TarsusReadStream.read_struct(1, "PlanDetail");
+        this.code = _TarsusReadStream.read_int(2);
+        this.message = _TarsusReadStream.read_string(3);
+    }
+};
+
+export class getUserPlansRes {
+    public data: Array<PlanDetail>;
+    public code: number;
+    public message: string;
+    public total: number;
+
+    constructor(...args: any[]) {
+        const _TarsusReadStream = new TarsusReadStream("getUserPlansRes", args);
+        this.data = _TarsusReadStream.read_list(1, "List<PlanDetail>");
+        this.code = _TarsusReadStream.read_int(2);
+        this.message = _TarsusReadStream.read_string(3);
+        this.total = _TarsusReadStream.read_int(4);
+    }
+};
+
+export class getPlanWordsByIdRes {
+    public data: PlanWords;
+    public code: number;
+    public message: string;
+
+    constructor(...args: any[]) {
+        const _TarsusReadStream = new TarsusReadStream("getPlanWordsByIdRes", args);
+        this.data = _TarsusReadStream.read_struct(1, "PlanWords");
+        this.code = _TarsusReadStream.read_int(2);
+        this.message = _TarsusReadStream.read_string(3);
     }
 };
